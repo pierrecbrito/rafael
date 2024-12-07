2ae7f859845239cfa08351487cf175642724c827
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-12-07 13:19:31 -0300

    Tela inicial

--- src/br/ufrn/eaj/tads/rafael/app/Main.java
+++ src/br/ufrn/eaj/tads/rafael/app/Main.java
@@ -3,11 +3,16 @@ package br.ufrn.eaj.tads.rafael.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 public class Main extends Application {
+	private double xOffset = 0;
+	private double yOffset = 0;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -15,10 +20,26 @@ public class Main extends Application {
 	            Scene scene = new Scene(root);
 	            primaryStage.setScene(scene);
 	            primaryStage.setTitle("Rafael - Gerenciamento de Hospital");
-	            primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/br/ufrn/eaj/tads/rafael/view/Logo-Rafael-removebg.png")));
+	            primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/br/ufrn/eaj/tads/rafael/view/favicon-wtbg.png")));
 	            primaryStage.setResizable(false);
 	            primaryStage.initStyle(javafx.stage.StageStyle.UNDECORATED);
 	            primaryStage.centerOnScreen();
+	            scene.setFill(Color.TRANSPARENT);
+	            
+	            primaryStage.initStyle(StageStyle.TRANSPARENT);
+	            primaryStage.setScene(scene);
+	         // Adicionar eventos de mouse para arrastar o Stage
+	            
+	            root.setOnMousePressed(event -> {
+	                xOffset = event.getSceneX();
+	                yOffset = event.getSceneY();
+	            });
+
+	            root.setOnMouseDragged(event -> {
+	                primaryStage.setX(event.getScreenX() - xOffset);
+	                primaryStage.setY(event.getScreenY() - yOffset);
+	            });
+	            
 	            primaryStage.show();
 	            
 		} catch(Exception e) {
