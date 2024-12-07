bb38273b2d31a01aa33a4344f84fe27f1a4d623c
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-12-07 02:31:34 -0300

    Start on the project

--- src/br/ufrn/eaj/tads/rafael/app/Main.java
+++ src/br/ufrn/eaj/tads/rafael/app/Main.java
@@ -1,20 +1,26 @@
 package br.ufrn.eaj.tads.rafael.app;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.image.Image;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			  	Parent root = FXMLLoader.load(getClass().getResource("/br/ufrn/eaj/tads/rafael/view/inicial.fxml"));
+	            Scene scene = new Scene(root);
+	            primaryStage.setScene(scene);
+	            primaryStage.setTitle("Rafael - Gerenciamento de Hospital");
+	            primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/br/ufrn/eaj/tads/rafael/view/Logo-Rafael-removebg.png")));
+	            primaryStage.setResizable(false);
+	            primaryStage.initStyle(javafx.stage.StageStyle.UNDECORATED);
+	            primaryStage.centerOnScreen();
+	            primaryStage.show();
+	            
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/br/ufrn/eaj/tads/rafael/controller/InicialController.java
@@ -0,0 +1,34 @@
+package br.ufrn.eaj.tads.rafael.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class InicialController {
+	@FXML
+	private TextField inputEmail;
+	@FXML
+	private PasswordField inputSenha;
+	@FXML
+	private Button btnEntrar;
+	
+	@FXML
+    private void initialize() {
+        // Inicialização, se necessário
+    }
+	
+
+    @FXML
+    private void entrar() {
+        String email = inputEmail.getText();
+        String senha = inputSenha.getText();
+        
+        if(email.equals("admin") && senha.equals("admin")) {
+        	System.out.println("Login realizado com sucesso!");
+        } else {
+        	System.out.println("Credenciais inválidas!");
+        }
+    }
+}
